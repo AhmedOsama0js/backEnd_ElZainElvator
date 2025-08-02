24512a0986d5c026d8d5a3ed2a18871bfb22af15
Ahmed Osama Awad Al-Saeed <135539823+AhmedOsama0js@users.noreply.github.com>
2025-08-02 17:08:51 +0300

    fixed 2

--- utils/telegramBot.js
+++ utils/telegramBot.js
@@ -2,6 +2,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const TOKEN = process.env.BOT_TOKEN;
 
-const telegramBot = new TelegramBot(TOKEN, { polling: false });
+const telegramBot = new TelegramBot(TOKEN);
 
 module.exports = { telegramBot };
