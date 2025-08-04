4fb467b9a568cfa6e3851d0e1af874fc76fcd327
Ahmed Osama Awad Al-Saeed <135539823+AhmedOsama0js@users.noreply.github.com>
2025-08-04 12:56:21 +0300

    new server

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const path = require("path");
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://lucateam.me3adbot.shop",
+  "https://uptowersaudi.com/",
 ];
 
 const app = express();
